b29268982996e9f2ca5e06a3228038dd8a82336c
Rbusis <csf170516@csf.edu.au>
2025-11-29 16:58:18 +0800

    Update debug.js

--- debug.js
+++ debug.js
@@ -1,8 +1,8 @@
 // debug.js
 export const DEBUG = {
-  global: false,      // Active debug pour tous les bots
-  autoselect: false,  // Debug AUTOSELECT
-  discovery: false,   // Debug DISCOVERY
-  swing: false,       // Debug SWING
-  degen: false        // Debug DEGEN
+  global: false,
+  autoselect: false,
+  discovery: false,
+  swing: false,
+  degen: false
 };
