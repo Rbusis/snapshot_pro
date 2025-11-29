0aa89f708d15e917013e6b7bb6ccbd4d35145bfd
Rbusis <csf170516@csf.edu.au>
2025-11-29 16:42:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // ===== DEBUG CONTROL (GLOBAL + PER BOT) =====
-// Nous importons simplement le fichier debug.js ici.
-// Plus rien ne doit se trouver après l'import.
 import { DEBUG } from "./debug.js";
 
 // index.js — CHEF D'ORCHESTRE ULTIME (MQI OFF)
@@ -12,8 +10,9 @@ import { startDiscovery } from "./discovery.js";
 import { startDegen } from "./degen.js";
 import { startSwing } from "./swing.js";
 
+
 // ========= KEEPALIVE RAILWAY =========
-const PORT = process.env.PORT || 8088;
+const PORT = process.env.PORT || 8080;
 
 http.createServer((req, res) => {
   res.writeHead(200);
